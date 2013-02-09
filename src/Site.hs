4e7cbeb68948002f7ac360308d563f92291c04ee
Favonia <favonia@gmail.com>
2013-02-09 17:25:10 -0500

    Enable dynamic reloading mode.

---
+++ src/Site.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TemplateHaskell #-}
+module Site where
+
+import Control.Lens
+
+import Snap
+
+import CharDatabase
+import EncodingTable
+import RestApi
+
+data App = App
+  { _charDatabase  :: Snaplet CharDatabase
+  , _encodingTable :: Snaplet EncodingTable
+  , _restApi       :: Snaplet RestApi
+  }
+
+$(makeLenses ''App)
+
+initApp :: SnapletInit App App
+initApp = makeSnaplet "app" "萌典校正系統" Nothing $ do
+  cs <- nestSnaplet "db"     charDatabase   $ initCharDatabase
+  es <- nestSnaplet "encode" encodingTable  $ initEncodingTable
+  as <- nestSnaplet "api"    restApi        $ initRestApi cs es
+  return $ App cs es as
+
