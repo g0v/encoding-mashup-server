c9e34d2d2d634f4b6ee6b202bd2df8be765c57fd
Favonia <favonia@gmail.com>
2013-02-09 23:18:44 -0500

    Refactor CharDatabase.

--- src/CharDatabase.hs
+++ src/CharDatabase.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleInstances #-}
 module CharDatabase where
 
 import Control.Applicative
 import Control.Monad (forM)
 import Control.Lens
 
+import Data.Maybe (listToMaybe)
 import Data.Text (Text)
 import Data.Int (Int64)
 import qualified Data.HashMap.Strict as H
@@ -18,62 +20,44 @@ import Snap.Snaplet.SqliteSimple
 
 import Type
 
+data CharPair = CharPair CharName CharInfo
 
-data CharInfoRow = CharInfoRow
-    { _charname_field :: CharName 
-    , _hidden_field :: !Bool
-    , _tabled_field :: !Bool
-    , _display_uni_field :: Maybe Text
-    , _display_ids_field :: Maybe Text
-    , _display_pua_field :: Maybe Text
-    , _exact_cns_field :: Maybe CnsCode
-    , _exact_manualuni_field :: Maybe Text
-    , _comments_field :: !Text
-    , _checked_field :: !Int64
-    , _timestamp_field :: !Int64
-    }
+unwrapCharPair :: CharPair -> (CharName, CharInfo)
+unwrapCharPair (CharPair a b) = (a, b)
 
-$(makeLenses ''CharInfoRow)
+instance FromRow CharPair where
+    fromRow = CharPair <$> field
+                       <*> (CharInfo <$> field
+                                     <*> field
+                                     <*> (CharDisplay <$> field
+                                                      <*> field
+                                                      <*> field)
+                                     <*> (CharExact <$> field
+                                                    <*> field)
+                                     <*> field
+                                     <*> field
+                                     <*> field)
 
-instance FromRow CharInfoRow where
-    fromRow = CharInfoRow <$> field <*> field <*> field <*> field <*> field
-                          <*> field <*> field <*> field <*> field <*> field
-                          <*> field
-
-
-data CharDatabase = CharDatabase { _db :: Snaplet Sqlite
-                                 }
-
-
+data CharDatabase = CharDatabase
+  { _db :: Snaplet Sqlite
+  }
 $(makeLenses ''CharDatabase)
-
 
 initCharDatabase :: SnapletInit b CharDatabase
 initCharDatabase = makeSnaplet "char-database" "字元資料庫" Nothing $ do
     d <- nestSnaplet "db" db sqliteInit
     return $ CharDatabase d
 
-
 getChars :: Handler b CharDatabase CharMap
-getChars = do 
-    rows <- with db $ query_ "select * from char_info" :: Handler b CharDatabase [CharInfoRow]
-    pairs <- forM rows $ \c_row ->
-                 return $ ((c_row ^. charname_field), (CharInfo (c_row ^. hidden_field) (c_row ^. tabled_field) (CharDisplay (c_row ^. display_uni_field)  (c_row ^. display_ids_field) (c_row ^. display_pua_field)) (CharExact (c_row ^. exact_cns_field) (c_row ^. exact_manualuni_field)) (c_row ^. comments_field) (c_row ^. checked_field) (c_row ^. timestamp_field)))
-    return $ H.fromList pairs
-
+getChars = H.fromList . map unwrapCharPair <$> access
+  where access = with db $ query_ "select * from char_info"
 
 getChar :: CharName -> Handler b CharDatabase (Maybe CharInfo)
-getChar c = do
-    rows <- with db $ query "select * from char_info where charname = ?" (Only c) :: Handler b CharDatabase [CharInfoRow]
-    case null rows of
-        True ->  return $ Nothing
-        False -> do let c_row = head rows
-                    return $ Just (CharInfo (c_row ^. hidden_field) (c_row ^. tabled_field) (CharDisplay (c_row ^. display_uni_field)  (c_row ^. display_ids_field) (c_row ^. display_pua_field)) (CharExact (c_row ^. exact_cns_field) (c_row ^. exact_manualuni_field)) (c_row ^. comments_field) (c_row ^. checked_field) (c_row ^. timestamp_field))
-
+getChar c = listToMaybe . map (snd . unwrapCharPair) <$> access
+  where access = with db $ query "select * from char_info where charname = ?" $ Only c
 
 updateChar :: CharName -> CharInfo -> Handler b CharDatabase ()
 updateChar cn ci = undefined undefined
-
 
 deleteChar :: CharName -> Handler b CharDatabase ()
 deleteChar = undefined
