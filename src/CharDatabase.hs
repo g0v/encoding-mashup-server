07586bf73f8b21cbd3ec47822f5bedbfa667850f
Yu-Cheng Ling <miaout17@gmail.com>
2013-02-11 17:17:24 +0800

    Bugfix for CharDatabase.hs
    1.Missing 'tabled' field in `toRow` function. Thanks my girlfriend.
      This is the first bug she found by tracing source code :)
    2.Fixed some syntax error in the SQL string.

--- src/CharDatabase.hs
+++ src/CharDatabase.hs
@@ -44,7 +44,19 @@ instance FromRow CharPair where
 instance ToRow CharPair where
     toRow cp = let cn = fst . unwrapCharPair $ cp
                    ci = snd . unwrapCharPair $ cp
-               in [toField cn, ci ^. hidden . to toField, ci ^. display . uni . to toField, ci ^. display . ids . to toField, ci ^. display . pua . to toField, ci ^. exact . cns . to toField, ci ^. exact . manualUni . to toField, ci ^. comments . to toField, ci ^. checked . to toField, ci ^. timestamp . to toField ]
+               in [
+                 toField cn,
+                 ci ^. hidden . to toField,
+                 ci ^. tabled . to toField,
+                 ci ^. display . uni . to toField,
+                 ci ^. display . ids . to toField,
+                 ci ^. display . pua . to toField,
+                 ci ^. exact . cns . to toField,
+                 ci ^. exact . manualUni . to toField,
+                 ci ^. comments . to toField,
+                 ci ^. checked . to toField,
+                 ci ^. timestamp . to toField
+               ]
 
 data CharDatabase = CharDatabase
   { _db :: Snaplet Sqlite
@@ -69,8 +81,8 @@ updateChar cn ci = do
     let cp = CharPair cn ci
     {- check out http://stackoverflow.com/questions/2717590/sqlite-upsert-on-duplicate-key-update -} 
     with db $ execute "INSERT OR IGNORE INTO char_info (charname, hidden, tabled, display_uni, display_ids, display_pua, exact_cns, exact_manualuni, comments, checked, timestamp) VALUES (?,?,?,?,?,?,?,?,?,?,?)" cp
-    with db $ execute "UPDATE char_info SET hidden=?, tabled=?, display_uni=?, display_ids=?, display_pua=?, exact_cns=?, exact_manualuni=?, comments=?, checked=?) where charname LIKE ?" ((ci ^. hidden), (ci ^. tabled), (ci ^. display . uni), (ci ^. display . ids), (ci ^. display. pua), (ci ^. exact . cns), (ci ^. exact . manualUni), (ci ^. comments), (ci ^. checked), cn)
-    with db $ execute "UPDATE char_info SET timestamp=?) where charname LIKE ?" ((ci ^. timestamp), cn)
+    with db $ execute "UPDATE char_info SET hidden=?, tabled=?, display_uni=?, display_ids=?, display_pua=?, exact_cns=?, exact_manualuni=?, comments=?, checked=? where charname LIKE ?" ((ci ^. hidden), (ci ^. tabled), (ci ^. display . uni), (ci ^. display . ids), (ci ^. display. pua), (ci ^. exact . cns), (ci ^. exact . manualUni), (ci ^. comments), (ci ^. checked), cn)
+    with db $ execute "UPDATE char_info SET timestamp=? where charname LIKE ?" ((ci ^. timestamp), cn)
 
 deleteChar :: CharName -> Handler b CharDatabase ()
 deleteChar cn = do
