6e53493df0552c6f2c31798e59d13b0c7bf73919
Favonia <favonia@gmail.com>
2013-02-18 15:48:33 -0500

    Move the HTTP code to a separate module.

--- src/RestApi.hs
+++ src/RestApi.hs
@@ -6,33 +6,21 @@ module RestApi
   , initRestApi
   ) where
 
-import           Prelude hiding (catch)
-------------------------------------------------------------------------------
-import           Data.Int (Int64)
-import qualified Data.Foldable as F
 import           Data.Maybe
 import           Data.Text (Text)
-import           Data.Text.Encoding
 import           Data.ByteString (ByteString)
 import qualified Data.ByteString.Lazy as LB
-import qualified Data.ByteString.Char8 as C8
 import qualified Data.HashMap.Strict as H
-import           Blaze.ByteString.Builder
-import           Data.Attoparsec
-import           Data.Attoparsec.ByteString.Char8
 import           Data.Aeson
 import           Data.Aeson.Types (parseMaybe)
 import           Data.Aeson.TH
 import           Control.Monad
-import           Control.Monad.CatchIO (catch)
 import           Control.Lens hiding ((.=))
-import           Crypto.Hash
 ------------------------------------------------------------------------------
 import           Snap
-import qualified Snap.Iteratee as I
-import           Snap.Internal.Parsing
 ------------------------------------------------------------------------------
 import           Type
+import           Utils
 import qualified CharDatabase as C
 import           EncodingTable
 
@@ -42,9 +30,6 @@ import           EncodingTable
 
 version :: Int
 version = 0
-
-maxBodyLen :: Int64
-maxBodyLen = 4096
 
 ------------------------------------------------------------------------------
 -- Haskell Datatype <-> JSON
@@ -65,131 +50,32 @@ instance ToJSON CharExact where
                         , "forced_uni" .= view forcedUni charexact
                         ]
 
-------------------------------------------------------------------------------
--- Etag
-------------------------------------------------------------------------------
-
-data NormalEtag = StrongNE Etag | WeakNE Etag deriving Eq
-data EtagPattern = WildE | NormalE [NormalEtag]
-
-pEtagPattern :: Parser EtagPattern
-pEtagPattern = pSpaces *> (pWildPattern <|> pNormalPatterns) <* pSpaces
-  where
-    pWildPattern = char '*' *> return WildE
-    pNormalPatterns = do
-      a <- pNormalPattern
-      b <- many (pSpaces *> char ',' *> pSpaces *> pNormalPattern)
-      return . NormalE $ a:b
-    pNormalPattern = choice
-      [ StrongNE <$> pQuotedString
-      , WeakNE <$> (string "W/" *> pQuotedString)
-      ]
-
-etagMatch :: EtagPattern -> Maybe Etag -> Bool
-etagMatch _           Nothing  = False
-etagMatch WildE       (Just _) = True
-etagMatch (NormalE l) (Just t) = StrongNE t `elem` l
-
-etag :: LB.ByteString -> Etag
-etag str = digestToHexByteString (hashlazy str :: Digest Skein256_256)
-
-quoteEtag :: Etag -> ByteString
-quoteEtag = C8.concatMap quote
-  where
-    quote '"' = "\""
-    quote x   = C8.singleton x
-
-------------------------------------------------------------------------------
--- Basic @finishWith@ and checking
-------------------------------------------------------------------------------
-
-err405 :: [Method] -> Handler b v a
-err405 allowed = finishWith
-  $ setResponseCode 405
-  $ setHeader "Allow" allowed'
-    emptyResponse
-  where
-    allowed' = C8.intercalate ", " $ map (C8.pack . show) allowed
-
-finishWithCode :: Int -> Maybe Etag -> Handler b v a
-finishWithCode code maybetag = finishWith
-  $ setResponseCode code
-  $ maybe id (setHeader "Etag" . quoteEtag) maybetag
-    emptyResponse
-
-err :: Int -> Handler b v a
-err code = finishWithCode code Nothing
-
-errIfNothing :: Int -> Maybe a -> Handler b v a
-errIfNothing code = maybe (err code) return
-
--- | Check HTTP 1.1 request headers.
-checkRequest :: Handler b v ()
-checkRequest = do
-  rq <- getRequest
-  when (isJust $ getHeader "Expect" rq) $ err 417
-
--- | Filter out Content-*.
---   TODO: Check @Content-Type@.
-checkPutRequest :: Handler b v ()
-checkPutRequest = do
-  rq <- getRequest
-  when (isJust $ getHeader "Content-Range" rq) $ err 501
-  when (isJust $ getHeader "Content-Encoding" rq) $ err 501
-  when (isJust $ getHeader "Content-Language" rq) $ err 501
-  when (isJust $ getHeader "Content-MD5" rq) $ err 501
-  when (isJust $ getHeader "Content-Range" rq) $ err 501
-
--- | @If-Match@, @If-None-Match@ and @If-Unmodified-Since@
-checkMatch :: Maybe Etag -> Handler b v ()
-checkMatch tag = do
-  rq <- getRequest
-  when (isJust $ getHeader "If-Unmodified-Since" rq) $ err 412
-  F.forM_ (getHeader "If-Match" rq) $ parseEP >=> ifMatchHandler
-  F.forM_ (getHeader "If-None-Match" rq) $ parseEP >=> ifNoneMatchHandler
-  where
-    parseEP = errIfNothing 400 . maybeResult . parse pEtagPattern
-    ifMatchHandler pat = unless (pat `etagMatch` tag) $ err 412
-    ifNoneMatchHandler pat = when (pat `etagMatch` tag) $
-      methods [GET, HEAD] (finishWithCode 304 tag) <|> err 412
-
 --------------------------------------------------------------------------------
--- JSON and ETag and version checking
+-- JSON tools.
 --------------------------------------------------------------------------------
 
-toJson' :: ToJSON a => a -> LB.ByteString
-toJson' = encode . toJSON
+jsonMime :: Mime
+jsonMime = "application/json"
 
-finishWithJson' :: LB.ByteString -> Maybe Etag -> Handler b v c
-finishWithJson' str tag = finishWith
-  $ setResponseBody body
-  $ maybe id (setHeader "ETag" . quoteEtag) tag
-  $ setContentType "application/json"
-    emptyResponse
-  where
-    body = I.enumBuilder $ fromLazyByteString str
+toLBS :: ToJSON a => a -> LB.ByteString
+toLBS = encode . toJSON
 
-readJson' :: Handler b v LB.ByteString
-readJson' = readRequestBody maxBodyLen
-  `catch`
-    \(_ :: I.TooManyBytesReadException) -> err 413
-
-fromJson' :: FromJSON a => LB.ByteString -> Handler b v a
-fromJson' = errIfNothing 400 . decode'
+fromLBS :: FromJSON a => LB.ByteString -> Handler b v a
+fromLBS = errIfNothing 400 . decode'
 
 readJson :: FromJSON a => Handler b v a
-readJson = fromJson' =<< readJson'
+readJson = fromLBS =<< readLBS
 
-readJsonMember :: FromJSON a => Object -> Text -> Handler b v a
-readJsonMember obj = errIfNothing 400 . parseMaybe (obj .:)
+jsonLookup :: FromJSON a => Object -> Text -> Handler b v a
+jsonLookup obj = errIfNothing 400 . parseMaybe (obj .:)
 
 checkVersion :: Object -> Handler b v ()
 checkVersion obj = do
-  version' <- readJsonMember obj "version"
+  version' <- jsonLookup obj "version"
   when (version' /= version) $ err 400
 
 --------------------------------------------------------------------------------
--- Snaplet state and initializer
+-- Snaplet state and initializer.
 --------------------------------------------------------------------------------
 
 data RestApi = RestApi
@@ -215,11 +101,11 @@ initRestApi cs es = makeSnaplet "rest-api" "JSON 介面" Nothing $ do
         _     -> logError "Capturing failed." >> pass
 
 --------------------------------------------------------------------------------
--- Handlers for CharInfo
+-- Handlers for CharInfo.
 --------------------------------------------------------------------------------
 
 frameChar :: CharInfo -> LB.ByteString
-frameChar c = toJson' $ object
+frameChar c = toLBS $ object
   [ "version"  .= version
   , "charinfo" .= c
   ]
@@ -234,22 +120,22 @@ charHandler charName = with charDatabase $
     getChar' = fmap frameChar <$> C.getChar charName
 
     getter = do
-      checkRequest
+      checkExpect
       -------------------------------------------
       output <- errIfNothing 404 =<< getChar'
       -------------------------------------------
       let tag = etag output
       checkMatch (Just tag)
       -------------------------------------------
-      finishWithJson' output (Just tag)
+      finishWithLBS jsonMime output (Just tag)
 
     setter = do
-      checkRequest
-      checkPutRequest
+      checkExpect
+      checkContent
       -------------------------------------------
       rq <- readJson
       checkVersion rq
-      charInfo <- readJsonMember rq "charinfo"
+      charInfo <- jsonLookup rq "charinfo"
       -------------------------------------------
       oldtag' <- fmap etag <$> getChar'
       checkMatch oldtag'
@@ -281,7 +167,7 @@ allCharsHandler = with charDatabase $
       charmap <- C.getChars
       -- FIXME: THIS MUST BE SLOW!
       let etagmap = H.map (etag . frameChar) charmap
-      let output = toJson' $ object
+      let output = toLBS $ object
             [ "version" .= version
             , "charmap" .= charmap
             , "etagmap" .= etagmap
@@ -290,7 +176,7 @@ allCharsHandler = with charDatabase $
       let tag = etag output
       checkMatch (Just tag)
       -------------------------------------------
-      finishWithJson' output (Just tag)
+      finishWithLBS jsonMime output (Just tag)
 
 {-
 updatedCharsHandler :: Handler b RestApi ()
---
+++ src/Utils.hs
@@ -0,0 +1,177 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{- |
+
+This module contains all the code to deal with HTTP 1.1
+that can be used in other projects.
+
+-}
+module Utils
+  (
+  -- * HTTP 1.1 tools.
+    err405
+  , finishWithCode
+  , err
+  , errIfNothing
+  , checkExpect
+  , checkContent
+  -- * 'Etag' manipulation and checking.
+  , etag
+  , checkMatch
+  -- * HTTP 1.1 IO.
+  , Mime
+  , maxBodyLen
+  , readLBS
+  , finishWithLBS
+  ) where
+
+import           Prelude hiding (catch)
+------------------------------------------------------------------------------
+import           Data.Int (Int64)
+import qualified Data.Foldable as F
+import           Data.Maybe
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString.Lazy as LB
+import qualified Data.ByteString.Char8 as C8
+import           Blaze.ByteString.Builder
+import           Data.Attoparsec
+import           Data.Attoparsec.ByteString.Char8
+import           Control.Monad
+import           Control.Monad.CatchIO (catch)
+import           Crypto.Hash
+------------------------------------------------------------------------------
+import           Snap
+import qualified Snap.Iteratee as I
+import           Snap.Internal.Parsing
+------------------------------------------------------------------------------
+import           Type
+
+------------------------------------------------------------------------------
+-- HTTP 1.1 tools specialized in JSON.
+------------------------------------------------------------------------------
+
+-- | HTTP 405. Needs to put allowed methods in the header @Allow@.
+err405 :: [Method] -> Handler b v a
+err405 allowed = finishWith
+  $ setResponseCode 405
+  $ setHeader "Allow" allowed'
+    emptyResponse
+  where
+    allowed' = C8.intercalate ", " $ map (C8.pack . show) allowed
+
+-- | Finish with an empty response, along with the code and the @ETag@ (if available).
+finishWithCode :: Int -> Maybe Etag -> Handler b v a
+finishWithCode code maybetag = finishWith
+  $ setResponseCode code
+  $ maybe id (setHeader "Etag" . quoteEtag) maybetag
+    emptyResponse
+
+-- | A specialized 'finishWithCode' without @ETag@.
+err :: Int -> Handler b v a
+err code = finishWithCode code Nothing
+
+-- | A convenience function to call 'err' when the result is 'Nothing'.
+errIfNothing :: Int -> Maybe a -> Handler b v a
+errIfNothing code = maybe (err code) return
+
+-- | Check the HTTP 1.1 header @Expect@.
+checkExpect :: Handler b v ()
+checkExpect = do
+  rq <- getRequest
+  when (isJust $ getHeader "Expect" rq) $ err 417
+
+-- | Check @Content-*@. This is required for the @PUT@ method.
+--   TODO: Take a MIME and check @Content-Type@.
+checkContent :: Handler b v ()
+checkContent = do
+  rq <- getRequest
+  when (isJust $ getHeader "Content-Range" rq) $ err 501
+  when (isJust $ getHeader "Content-Encoding" rq) $ err 501
+  when (isJust $ getHeader "Content-Language" rq) $ err 501
+  when (isJust $ getHeader "Content-MD5" rq) $ err 501
+  when (isJust $ getHeader "Content-Range" rq) $ err 501
+
+------------------------------------------------------------------------------
+-- Etag
+------------------------------------------------------------------------------
+
+-- | The data type for the content of the headers @If-Match@ and @If-None-Match@.
+data NormalEtag = StrongNE Etag | WeakNE Etag deriving Eq
+data EtagPattern = WildE | NormalE [NormalEtag]
+
+-- | The parser for the headers @If-Match@ and @If-None-Match@.
+pEtagPattern :: Parser EtagPattern
+pEtagPattern = pSpaces *> (pWildPattern <|> pNormalPatterns) <* pSpaces
+  where
+    pWildPattern = char '*' *> return WildE
+    pNormalPatterns = do
+      a <- pNormalPattern
+      b <- many (pSpaces *> char ',' *> pSpaces *> pNormalPattern)
+      return . NormalE $ a:b
+    pNormalPattern = choice
+      [ StrongNE <$> pQuotedString
+      , WeakNE <$> (string "W/" *> pQuotedString)
+      ]
+
+-- | Matching for @ETags@.
+etagMatch :: EtagPattern -> Maybe Etag -> Bool
+etagMatch _           Nothing  = False
+etagMatch WildE       (Just _) = True
+etagMatch (NormalE l) (Just t) = StrongNE t `elem` l
+
+-- | A function to generate @ETag@ for a body.
+etag :: LB.ByteString -> Etag
+etag str = digestToHexByteString (hashlazy str :: Digest Skein256_256)
+
+-- | A function to quote an @ETag@ according to the standard.
+quoteEtag :: Etag -> ByteString
+quoteEtag = C8.concatMap quote
+  where
+    quote '"' = "\""
+    quote x   = C8.singleton x
+
+-- | Check @If-Match@, @If-None-Match@ and @If-Unmodified-Since@
+--   with respect to the @ETag@ of the resource on the server.
+--   'Nothing' is given if the requested resource does not exist.
+--   The check is still required in that case because HTTP 1.1 allows
+--   wild patterns.
+--
+--   We always reject @If-Unmodified-Since@ because we don't use timestamp.
+checkMatch :: Maybe Etag -> Handler b v ()
+checkMatch tag = do
+  rq <- getRequest
+  when (isJust $ getHeader "If-Unmodified-Since" rq) $ err 412
+  F.forM_ (getHeader "If-Match" rq) $ parseEP >=> ifMatchHandler
+  F.forM_ (getHeader "If-None-Match" rq) $ parseEP >=> ifNoneMatchHandler
+  where
+    parseEP = errIfNothing 400 . maybeResult . parse pEtagPattern
+    ifMatchHandler pat = unless (pat `etagMatch` tag) $ err 412
+    ifNoneMatchHandler pat = when (pat `etagMatch` tag) $
+      methods [GET, HEAD] (finishWithCode 304 tag) <|> err 412
+
+------------------------------------------------------------------------------
+-- HTTP 1.1 IO specilized in JSON.
+------------------------------------------------------------------------------
+
+-- | The type of a base MIME. Subjects to changes.
+type Mime = ByteString
+
+-- | Maximum length for the request body.
+maxBodyLen :: Int64
+maxBodyLen = 4096
+
+-- | Read the HTTP request up to 'maxBodyLen'.
+readLBS :: Handler b v LB.ByteString
+readLBS = readRequestBody maxBodyLen
+  `catch`
+    \(_ :: I.TooManyBytesReadException) -> err 413
+
+-- | Finish with the given body immediately.
+finishWithLBS :: Mime -> LB.ByteString -> Maybe Etag -> Handler b v c
+finishWithLBS mime str tag = finishWith
+  $ setResponseBody body
+  $ maybe id (setHeader "ETag" . quoteEtag) tag
+  $ setContentType mime
+    emptyResponse
+  where
+    body = I.enumBuilder $ fromLazyByteString str
