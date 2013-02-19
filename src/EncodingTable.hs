d8b8e8324b0c650df9700f5b45ffb61df208a0cd
Favonia <favonia@gmail.com>
2013-02-19 09:50:55 -0500

    Skeleton for `EncodingTable`.

--- src/EncodingTable.hs
+++ src/EncodingTable.hs
@@ -1,27 +1,49 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
-module EncodingTable where
+module EncodingTable
+  (
+    EncodingTable
+  , initEncodingTable
+  , cnsCodeToUniChar
+  , uniCharToCnsCode
+  , isBlackListed
+  ) where
 
-import Data.HashMap.Strict (HashMap, empty, fromList)
-import Data.Text (Text)
-import Control.Lens
-import Snap.Core
-import Snap.Snaplet
-
-import Type
+import           System.FilePath
+import           Data.Maybe
+import           Control.Applicative
+import qualified Data.ByteString.Lazy as LB
+import           Data.HashMap.Strict (HashMap)
+import           Control.Monad.IO.Class (liftIO)
+import           Data.Aeson
+import           Control.Lens
+------------------------------------------------------------------------------
+import           Snap.Snaplet
+------------------------------------------------------------------------------
+import           Type
 
 data EncodingTable = EncodingTable
-  { _cnsTable :: HashMap CnsCode Text
-  , _uniTable :: HashMap Text CnsCode
+  { _cns2uniTable :: HashMap CnsCode UniChar
+  , _uni2cnsTable :: HashMap UniChar CnsCode
   }
 $(makeLenses ''EncodingTable)
 
 initEncodingTable :: SnapletInit b EncodingTable
 initEncodingTable = makeSnaplet "encoding-table" "Unicode 和 Cns 表格" Nothing $ do
-  return $ EncodingTable empty empty
+  snapletDir <- getSnapletFilePath
+  cns2uni <- liftIO $ fromJust . decode' <$> LB.readFile (snapletDir </> "cns2uni.json")
+  uni2cns <- liftIO $ fromJust . decode' <$> LB.readFile (snapletDir </> "uni2cns.json")
+  return $ EncodingTable cns2uni uni2cns
 
-lookupUnicode :: CnsCode -> Handler b EncodingTable (Maybe Text)
-lookupUnicode uni = use $ uniTable.at uni
+-- | CNS-11643 code to Unicode code point(s).
+cnsCodeToUniChar :: CnsCode -> Handler b EncodingTable (Maybe UniChar)
+cnsCodeToUniChar code = use $ uni2cnsTable.at code
 
-lookupCns :: Text -> Handler b EncodingTable (Maybe CnsCode)
-lookupCns cns = use $ cnsTable.at cns
+-- | Unicode code point(s) to CNS-11643 code.
+uniCharToCnsCode :: UniChar -> Handler b EncodingTable (Maybe CnsCode)
+uniCharToCnsCode char = use $ cns2uniTable.at char
+
+-- | (NOT IMPLEMENTED YET!)
+--   A function to test whether the leading code point is a compatible ideograph.
+isBlackListed :: UniChar -> Bool
+isBlackListed _ = False
