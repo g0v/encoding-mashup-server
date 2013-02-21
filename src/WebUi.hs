d84a6355f8b9e6cc00f5aae6f760b6fd6a24bd46
Favonia <favonia@gmail.com>
2013-02-21 11:27:21 -0500

    Generate CSS files on the fly.

--- src/WebUi.hs
+++ src/WebUi.hs
@@ -6,22 +6,62 @@ module WebUi
   ) where
 
 import           Data.String
+import           Data.ByteString
 import           System.FilePath
+import           Data.Text.Lazy.Encoding (encodeUtf8)
+import           Clay
 ------------------------------------------------------------------------------
-import           Snap.Snaplet
+import           Snap
 import           Snap.Util.FileServe
 ------------------------------------------------------------------------------
 import           Util
+import           HttpUtil
 
 data WebUi = WebUi
 
 initWebUi :: SnapletInit b WebUi
 initWebUi = makeSnaplet name description getData $ do
-  snapletDir <- getSnapletFilePath
-  addRoutes [("", serveDirectory $ snapletDir </> "static")]
+  addRoutes [ ("css", pathArg cssHandler)
+            , ("",            staticHandler)
+            ]
   return WebUi
   where
     name :: IsString a => a
     name = "web-ui"
     description = "Web-based User Interface"
     getData = Just $ getResourceDir name
+
+staticHandler :: Handler b WebUi ()
+staticHandler = do
+  snapletDir <- getSnapletFilePath
+  serveDirectory $ snapletDir </> "static"
+
+cssHandler :: ByteString -> Handler b WebUi ()
+cssHandler fp = case fp of
+  "encoding-mashup.css" -> finishWithCss encodingMashupCss
+  _                     -> pass
+  where
+    cssMime = "text/css"
+
+    finishWithCss css = do
+      let out = encodeUtf8 $ renderWith compact [] css
+      finishWithLBS cssMime out (Just $ etag out)
+
+encodingMashupCss :: Css
+encodingMashupCss = do 
+  star # byClass "char-info" ? do
+    margin 2 2 2 2
+    height $ px 40
+    float sideLeft
+
+  star # byClass "char-info-display" ? do
+    float sideLeft 
+    fontSize $ px 24
+    lineHeight $ px 24
+    height $ px 30
+    width $ px 100
+    border solid (px 2) black
+    borderRadius $ px 8
+
+  input ? do
+    float sideLeft
