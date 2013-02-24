6a942b0ffc06b639af801d6b13aeee71d46a5eef
Favonia <favonia@gmail.com>
2013-02-24 11:52:07 -0500

    Shorter names for executives.

--- scripts/execute_snap.sh
+++ scripts/execute_snap.sh
@@ -2,4 +2,4 @@
 
 # Since keter automatically choose binding port on behalf of server process and pass the chosen one by setting $PORT env variable, 
 # we have to add this shell script layer to receive the binding port then pass to the server.
-exec dist/build/encoding-mashup-server/encoding-mashup-server -p $PORT
+exec dist/build/em-server/em-server -p $PORT
