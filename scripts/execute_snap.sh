23300385ed2f60a2ee9bc4f31356777eb2a6cdd1
Paul Meng <mno2@mno2.org>
2013-02-24 15:47:55 +0800

    Keter only accept config/ as the folder containing yaml at this time

---
+++ scripts/execute_snap.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Since keter automatically choose binding port on behalf of server process and pass the chosen one by setting $PORT env variable, 
+# we have to add this shell script layer to receive the binding port then pass to the server.
+exec dist/build/encoding-mashup-server/encoding-mashup-server -p $PORT
