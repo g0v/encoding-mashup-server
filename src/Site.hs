d8162b0d5d9696cfcb27ff9729af5083ac38c380
Paul Meng <mno2@mno2.org>
2013-05-15 00:31:29 +0800

    Redirect to web/index.html when accessing the root

--- src/Site.hs
+++ src/Site.hs
@@ -23,9 +23,12 @@ $(makeLenses ''App)
 
 initApp :: SnapletInit App App
 initApp = makeSnaplet "app" "萌典校正系統" Nothing $ do
+  addRoutes [("", indexHandler)]
   cs <- nestSnaplet "db"  charDatabase    initCharDatabase
   es <- nestSnaplet "tbl" encodingTable   initEncodingTable
   ws <- nestSnaplet "web" webUi           initWebUi
   as <- nestSnaplet "api" restApi       $ initRestApi cs es
   return $ App cs es ws as
 
+indexHandler :: Handler b App ()
+indexHandler = finishWith $ setResponseCode 301 $ setHeader "Location" "web/index.html" $ emptyResponse
